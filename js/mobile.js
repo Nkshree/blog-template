25ba88c5cd0bf2ed73a9759f7de3b5f803d4614b
Nkshree <nkshreeharsha@gmail.com>
2024-06-21 00:06:11 +0530

    added scrol-to-top button

--- js/mobile.js
+++ js/mobile.js
@@ -4,3 +4,23 @@ const navMenu = document.getElementById('navMenu');
 navToggle.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
+
+const goTopBtn = document.querySelector('.go-top-btn');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 150) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+    
+  })
+})
